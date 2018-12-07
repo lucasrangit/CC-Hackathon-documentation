ded0f68e7036585f7d40ea492a58bedd519850d7
benjaminbach <benjamin.bach@mydaco.com>
2018-12-07 16:23:29 +0100

    update example service

--- Example Service/src/main.js
+++ Example Service/src/main.js
@@ -1,148 +1,151 @@
-const { getPowerDevices, getBenchmark } = require('./core');
-const { continousInformation, kWhToEuro } = require('./powerAnalyze');
 const { translations } = require('./translations');
-const { deviceIframe } = require('./selectionTemplate');
-const { benchmarkData } = require('./benchmarkData');
-const { mailTemplate: mailTemplateDE } = require('./mailTemplateDE');
-const { mailTemplate: mailTemplateEN } = require('./mailTemplateEN');
+
+const { initWidget } = require('./iframes/initWidget');
+
+const { finishWidget } = require('./iframes/finishWidget');
 
 const mydaco = require('mydaco');
 
-exports.main = async function (params) {
-  try {
-    // If not triggered by cron job.
-    if (params.inter !== 'Cron') {
-      const { lang = 'en' } = params.params;
-      const t = translations[lang];
-      const { title } = t.iFrameText;
-      const templateId = 'v1';
-      if (params.params.initAction) {
-        return await handleInit(t, title, templateId, lang);
-      }
-      return await handleIframeClosed(params, t, title, lang, templateId);
+async function main(params) {
+    console.log('started');
+    try {
+        console.log(params);
+
+        if (params.inter === 'Widget') {
+            const { lang = 'en' } = params.params;
+            return await getWidget(lang);
+        }
+
+        if (params.inter === 'ServiceMarketplace') {
+            const { lang = 'en' } = params.params;
+            const t = translations[lang];
+
+            // save language for late
+            await mydaco.interface('KeyValueStore', 'put', { key: 'lang', value: lang });
+
+            // go to things like settings and so on
+            return await handleInteraction(params.params, t);
+        }
+
+    } catch (error) {
+        console.log('main error');
+        console.log(error);
+        return { html: 'An undefined error occured.' };
     }
-    return await handleCron();
-  } catch (error) {
-    return {};
-  }
 }
 
-async function handleInit(t, title, templateId, lang) {
-  const customId = '1';
-  // Get all devices of the user, that are suitable for energy benchmarks.
-  const devices = await getPowerDevices(lang);
-  if (devices.length === 0) {
-    // It doesn't make sense to start the service, if the user doesn't have suitable devices.
-    const lastIFrame = true;
-    return { html: t.htmlNoDevice, title, lastIFrame, customId, lang };
-  }
-  // Store the device for later use.
-  await mydaco.interface('KeyValueStore', 'put', { key: 'devices', value: devices });
-  const pugFill = { devices, ...t.iFrameText };
-  const pugCode = deviceIframe;
-  const lastIFrame = false;
-  return {
-    pugCode, pugFill, title, templateId, lastIFrame, customId, lang,
-  };
+// handle the interaction on the iframe
+async function handleInteraction(params, t) {
+    console.log('iframe interaction');
+    console.log(params);
+    // widget setup done
+    if (params.inputs) {
+        // handle Nothing selected
+        if (!params.inputs.selectedDevice || params.inputs.selectedDevice === "") {
+            console.log("no device selected");
+            return await handleWidgetInit(t, "noDevices");
+        }
+        // widget setup done
+     
+        return finishWidgetInit(t, params);
+
+    }
+    return await handleWidgetInit(t);
 }
 
-async function handleIframeClosed(params, t, title, lang, templateId) {
-  const { fields } = params.params;
-  fields.lang = lang;
-  const lastIFrame = true;
-  const customId = '2';
+async function finishWidgetInit(t, params) {
+    console.log('iframe for finish widget');
+    console.log(params);
+    const value = {
+        device: params.inputs.selectedDevice,
+    };
+    await mydaco.interface('KeyValueStore', 'put', { key: 'widgetSettings', value });
 
-  //Save fields for later use.
-  await mydaco.interface('KeyValueStore', 'put', { key: 'fields', value: fields });
-  // Create cron pattern 'every minute'. We don't do the calculation just in time, because that would delay the display of the iframe by a couple of seconds.
-  await mydaco.interface('Cron', 'put', { cronPatterns: ['* * * * *'] });
-  return { html: t.htmlSuccess, title, templateId, lastIFrame, customId, lang };
+    const html = finishWidget(t);
+    return { html, title: t.title };
 }
 
-async function handleCron() {
-  try {
-    // Remove cron pattern (Otherwise this action would be called every minute till the end of time)
-    await mydaco.interface('Cron', 'put', { cronPatterns: [] });
-    // Retrieve alle data, that has been saved in the previous steps.
-    const keyValue = await mydaco.interface('KeyValueStore', 'get', { key: 'fields' });
-    const devicesDb = await mydaco.interface('KeyValueStore', 'get', { key: 'devices' });
-    // Delete all data from KeyValueStore.
-    await mydaco.interface('KeyValueStore', 'remove', { key: 'fields' });
-    await mydaco.interface('KeyValueStore', 'remove', { key: 'devices' });
+// iframe to select devices, fixed time span of 1 week for the moment
+async function handleWidgetInit(t, error = "") {
+    // get devices
+    const devices = await getPowerDevices();
+    // not checked for existence, should only be here, if devices exist
 
-    const fields = keyValue.value;
-    const { lang = 'en' } = fields;
-    const t = translations[lang];
-    // Retrieve data and calculate the energy benchmark.
-    const benchmark = await getBenchmark(fields, devicesDb.value);
+    // error can occur from no device selected
+    const html = initWidget(t, devices, error);
+    return { html, title: t.title };
+}
 
-    // Prepare mail to user.
-    const subject = t.mailSubject;
-    let text;
-    let html;
-    if (benchmark.length === 0) {
-      text = t.mailError;
-      html = t.mailError;
-    } else {
-      const start = new Date(fields.start);
-      const end = new Date(fields.end);
-      const information = continousInformation(start, end, benchmark);
 
-      const startString = `${start.getDate()}.${start.getMonth() + 1}.${start.getFullYear()}`;
-      const endString = `${end.getDate()}.${end.getMonth() + 1}.${end.getFullYear()}`;
 
-      const { mailRating } = t;
-      let rating;
-      // Compare it to existing energy benchmark data and rate the fridge accordingly.
-      if (information.normConsumption < benchmarkData['0.1']) {
-        rating = mailRating.veryLow;
-      } else if (information.normConsumption < benchmarkData['0.4']) {
-        rating = mailRating.low;
-      } else if (information.normConsumption < benchmarkData['0.6']) {
-        rating = mailRating.average;
-      } else if (information.normConsumption < benchmarkData['0.9']) {
-        rating = mailRating.high;
-      } else {
-        rating = mailRating.veryHigh;
-      }
+async function getPowerDevices() {
+    const devs = await mydaco.interface('IotCore', 'powerDevices', {});
+    console.log(devs);
+    const devices = devs.devices;
+    const deviceInformation = devices.map(device => ({
+        displayName: device.friendlyName ? device.friendlyName : device.readableName,
+        id: device.id,
+    })
+    );
+    return deviceInformation;
+}
 
-      benchmarkData['0.5'] = (benchmarkData['0.4'] + benchmarkData['0.6']) / 2;
+/////////////////////////////////
+//////// WIDGET
+/////////////////////////////////
 
-      const replacements = {
-        "%RATING%": rating,
-        "%FROMDATE%": startString,
-        "%TODATE%": endString,
-        "%DAYS%": information.days,
-        "%MONTHCONSUMPTION%": information.normConsumption.toFixed(2),
-        "%MONTHCONSUMPTIONEURO%": information.normConsumptionEuro.toFixed(2),
-        "%MINFE%": benchmarkData['0.1'].toFixed(2),
-        "%MINEUROFE%": kWhToEuro(benchmarkData['0.1']).toFixed(2),
-        "%MAXFE%": benchmarkData['0.9'].toFixed(2),
-        "%MAXFEEURO%": kWhToEuro(benchmarkData['0.9']).toFixed(2),
-        "%AVERAGEFE%": benchmarkData['0.5'].toFixed(2),
-        "%AVERAGEEUROFE%": kWhToEuro(benchmarkData['0.5']).toFixed(2),
-        "%TOTALCONSUMPTION%": information.totalConsumption.toFixed(2)
-      };
-      let filledMail = (lang === 'en') ? mailTemplateEN : mailTemplateDE;
-      // Fill in placeholders in the mailTemplate.
-      filledMail = filledMail.replace(/%\w+%/g, function (all) {
-        return replacements[all] || all;
-      });
 
-      html = filledMail;
-      text = html;
+async function getWidget(lang) {
+    const settings = await mydaco.interface('KeyValueStore', 'get', { key: 'widgetSettings' });
+    const start = Math.floor(new Date().getTime() / 86400000) * 86400000;
+    // end date needs to have 1 day added, to bascially calculate till 23:59
+    const end = start + 86400000 - 1000;
+
+    const parameters = {
+        id: 'test',//settings.value.device,
+        start,
+        end,
+        grouping: 'minute',
+        aggregate: 'mean'
+    };
+
+    const data = await mydaco.interface('IotCore', 'powerData', parameters);
+
+    const events = [];
+    let lastEvent = '';
+    const THRESHOLD = 10;
+    for (const point of data.series) {
+        if (point[1] > THRESHOLD && lastEvent !== 'ON') {
+            lastEvent = 'ON';
+            events.push({ time: point[0], event: lastEvent });
+        } else if (point[1] <= THRESHOLD && lastEvent !== 'OFF') {
+            lastEvent = 'OFF';
+            events.push({ time: point[0], event: lastEvent });
+        }
+    }
+    let html = 'Power : ';
+    for (const event of events) {
+        const date = new Date(event.time);
+        html += '<br>' + niceTime(date) + ' - ' + event.event;
     }
 
-    await writeEmail(subject, text, html);
-    return {};
-  } catch (error) {
-    // Inform the user if something went wrong.
-    await writeEmail(t.mailErrorSubject, t.mailError, t.mailError);
-  }
+    return { html };
 }
 
-//Write mail the the user with the energy benchmark.
-function writeEmail(subject, text, html) {
-  const parameters = { subject, text, html };
-  return mydaco.interface('Mail', 'sendmail', parameters);
+function niceTime(date) {
+    const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+    const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+    return hours + ':' + minutes;
 }
+
+async function setWidget(benchmark) {
+    console.log('set benchmark ', benchmark)
+    // only save the benchmark, display stuff will be done later on
+    await mydaco.interface('KeyValueStore', 'put', { key: 'benchmark', value: benchmark });
+    return { success: true };
+}
+
+
+module.exports = {
+    main,
+};
--- Example Service/src/translations.js
+++ Example Service/src/translations.js
@@ -1,49 +1,145 @@
 const translations = {
-  en: {
-    iFrameText: {
-      title: "Fresh Energy Fridge Benchmark",
-      selectDeviceText: "Please select your fridge measurement device.",
-      dayToStartText: 'Day to start analysis',
-      dayToEndText: 'To',
-    },
-    htmlNoDevice: '<p>We are sorry, you seem to have no suitable device.</p>',
-    htmlError: '<p>We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?</p>',
-    htmlSuccess: '<p>Thank you! You will receive your benchmark by email soon!</p>',
-    mailSubject: 'Your energy benchmark arrived',
-    mailErrorSubject: 'Problems with your energy benchmark',
-    mailError: 'We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?',
-    mailRating: {
-      veryLow: 'very low',
-      low: 'low',
-      average: 'average',
-      high: 'high',
-      veryHigh: 'very high'
-    }
-  },
-  de: {
-    iFrameText: {
-      title: "Fresh Energy Fridge Benchmark",
-      selectDeviceText: "Please select your fridge measurement device.",
-      dayToStartText: 'Day to start analysis',
-      dayToEndText: 'To',
-    },
-    htmlNoDevice: '<p>We are sorry, you seem to have no suitable device.</p>',
-    htmlError: '<p>We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?</p>',
-    htmlSuccess: '<p>Thank you! You will receive your benchmark by email soon!</p>',
-    mailSubject: 'Your energy benchmark arrived',
-    mailErrorSubject: 'Problems with your energy benchmark',
-    mailError: 'We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?',
-    mailRating: {
-      veryLow: 'sehr gering',
-      low: 'gering',
-      average: 'mittel',
-      high: 'hoch',
-      veryHigh: 'sehr hoch'
-    }
+	"en": {
+		title: "Fresh Compare for fridge",
+		initIFrame: {
+			noDeviceTitle: "We are sorry.",
+			noDeviceText: "you seem to have no suitable device. Please use an electricity metering device (smart plug) from mystrom, Sygonix or Zipato to use our service!",
+			selectTitle: "What do you want to do?",
+			selectText: "You can choose whether you want to receive an email with an analysis or you want to configure the widget.",
+			textMail: "benchmark by email",
+			textWidget: "widget configuration",
+			buttonLabel: "Proceed",
+			errorType: "Please select what you intend to do!",
+		},
+		deviceSelection: {
+			title: "Select a device",
+			text: "Please select the device which is measuring the electricity consumption of your fridge:",
+			error: "You have to select a device",
+			buttonLabel: "Proceed"
+		},
+		widgetFinished: {
+			title: "Widget installed",
+			text: "Your widget is now installed and you can start to monitor your power consumption!",
+			error: "There has been trouble to start your widget.",
+			buttonLabel: "Close"
+		},
+		mailSettings: {
+			title: "Benchmark settings",
+			daySelectionText: "Please select the time range of the consumption data which you would like to analyze:",
+			dayToStartText: 'From',
+			dayToEndText: 'To',
+			buttonLabel: "Proceed",
+			error: "You have to select valid dates"
+		},
+		mailFinished: {
+			title: "Benchmark started",
+			text: "Your benchmark started and you will receive an email soon!",
+			error: "There has been trouble to start your benchmark.",
+			buttonLabel: "Close"
+		},
+		mail: {
+			subject: 'Your Fresh Compare energy benchmark',
+			errorSubject: 'Problems with your energy benchmark',
+			error: 'We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?',
+			rating: {
+				veryLow: 'very good',
+				low: 'good',
+				average: 'average',
+				high: 'bad',
+				veryHigh: 'very bad'
+			}
+		},
+		widget: {
+			noData: "Sorry. There is no benchmark available yet.",
+			error: 'We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?',
+			rating: {
+				veryLow: 'Very Good',
+				low: 'Good',
+				average: 'Average',
+				high: 'Bad',
+				veryHigh: 'Very Bad'
+			},
+			image: {
+				veryLow: 'ScaleA+++.png',
+				low: 'ScaleA+.png',
+				average: 'ScaleA.png',
+				high: 'ScaleB.png',
+				veryHigh: 'ScaleD.png'
+			}
+		},
 
-  },
+	},
+	"de": {
+		title: "Fresh Compare for fridge",
+		initIFrame: {
+			noDeviceTitle: "We are sorry.",
+			noDeviceText: "you seem to have no suitable device. Please use an electricity metering device (smart plug) from mystrom, Sygonix or Zipato to use our service!",
+			selectTitle: "What do you want to do?",
+			selectText: "You can choose whether you want to receive an email with an analysis or you want to configure the widget.",
+			textMail: "benchmark by email",
+			textWidget: "widget configuration",
+			buttonLabel: "Proceed",
+			errorType: "Please select what you intend to do!",
+		},
+		deviceSelection: {
+			title: "Select a device",
+			text: "Please select the device which is measuring the electricity consumption of your fridge:",
+			error: "You have to select a device",
+			buttonLabel: "Proceed"
+		},
+		widgetFinished: {
+			title: "Widget installed",
+			text: "Your widget is now installed and you can start to monitor your power consumption!",
+			error: "There has been trouble to start your widget.",
+			buttonLabel: "Close"
+		},
+		mailSettings: {
+			title: "Benchmark settings",
+			daySelectionText: "Please select the time range of the consumption data which you would like to analyze:",
+			dayToStartText: 'From',
+			dayToEndText: 'To',
+			buttonLabel: "Proceed",
+			error: "You have to select valid dates"
+		},
+		mailFinished: {
+			title: "Benchmark started",
+			text: "Your benchmark started and you will receive an email soon!",
+			error: "There has been trouble to start your benchmark.",
+			buttonLabel: "Close"
+		},
+		mail: {
+			subject: 'Your Fresh Compare energy benchmark',
+			errorSubject: 'Problems with your energy benchmark',
+			error: 'We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?',
+			rating: {
+				veryLow: 'very good',
+				low: 'good',
+				average: 'average',
+				high: 'bad',
+				veryHigh: 'very bad'
+			}
+		},
+		widget: {
+			noData: "Sorry. There is no benchmark available yet.",
+			error: 'We are sorry! We ran into problems and were not able to analyze your data. Maybe there is no suitable device connected?',
+			rating: {
+				veryLow: 'Very Good',
+				low: 'Good',
+				average: 'Average',
+				high: 'Bad',
+				veryHigh: 'Very Bad'
+			},
+			image: {
+				veryLow: 'ScaleA+++.png',
+				low: 'ScaleA+.png',
+				average: 'ScaleA.png',
+				high: 'ScaleB.png',
+				veryHigh: 'ScaleD.png'
+			}
+		},
+	}
 };
 
 module.exports = {
-  translations
+	translations
 };
